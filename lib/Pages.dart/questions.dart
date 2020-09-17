a09eb671023afbec2a46b00b821ce1b40df0306c
Shazia Muskaan <shaz.muskaan1123@gmail.com>
2020-09-17 11:27:02 +0530

    Update questions.dart

--- lib/Pages.dart/questions.dart
+++ lib/Pages.dart/questions.dart
@@ -1,33 +1,80 @@
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
+//import 'package:universal_html/html.dart' as html;
 import './home.dart';
+//import 'package:http/http.dart' as http;
 
-class QuestionPapers extends StatelessWidget {
+class QuestionPapers extends StatefulWidget {
+  final globaldata;
+  QuestionPapers(this.globaldata);
+  @override
+  _QuestionPapersState createState() => _QuestionPapersState(globaldata);
+}
+
+class _QuestionPapersState extends State<QuestionPapers> {
+  final globaldata;
+  _QuestionPapersState(this.globaldata);
+  var keys;
+  var values;
+
+  makeList() {
+    keys = globaldata.keys.toList();
+    values = globaldata.values.toList();
+    print(keys);
+  }
+
   @override
   Widget build(BuildContext context) {
+    makeList();
+
     return MaterialApp(
-        theme: ThemeData(
-            brightness: Brightness.light, primarySwatch: Colors.lightBlue),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.lightBlue[300],
-          appBar: AppBar(
-            backgroundColor: Colors.lightBlue[700],
-            title: Center(
-                child: Text('Question Papers',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold))),
-            leading: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Home(), fullscreenDialog: true));
-              },
-              child: Icon(Icons.arrow_back, color: Colors.white),
-            ),
+      theme: ThemeData(
+          brightness: Brightness.light, primarySwatch: Colors.lightBlue),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[300],
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue[700],
+          title: Center(
+              child: Text('Question Papers',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold))),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => Home(), fullscreenDialog: true));
+            },
+            child: Icon(Icons.arrow_back, color: Colors.white),
           ),
-        )
-        );
+        ),
+        body: Center(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: keys.length,
+            itemBuilder: ((context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: RaisedButton(
+                  child: Text('${keys[index]}'),
+                  onPressed: () async {
+                    var url = values[index];
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                    //html.window.open(values[index],'papers');
+                    //await launch(values[index]);
+                  },
+                ),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
